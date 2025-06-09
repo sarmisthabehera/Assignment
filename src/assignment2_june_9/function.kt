ef8e6f8cd1feb47054e78de77167b1a22de14138
sarmisthabehera <eie.23beii12@silicon.ac.in>
2025-06-09 21:03:51 +0530

    assignment2 june 9

---
+++ src/assignment2_june_9/function.kt
@@ -0,0 +1,21 @@
+package assignment2_june_9
+fun main()
+{
+// Functions
+println("\n6. Sum of odd numbers:")
+println(sumOfOdds(list1))
+
+println("\n7. Sum of 5 and 10:")
+println(sumDescription(5, 10))
+
+println("\n8. Full Name:")
+println(formatName("John", "Doe"))
+println(formatName())
+
+println("\n9. Factorial of 5:")
+println(factorial(5))
+
+println("\n10. Longest string in the list:")
+val stringList = listOf("Kotlin", "Java", "JavaScript", "C")
+println(longestString(stringList))
+}
