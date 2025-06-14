ad754b8a841d3f2a2a73792c30b7d84bef4a508a
sarmisthabehera <eie.23beii12@silicon.ac.in>
2025-06-14 13:22:46 +0530

    assignment3 june 13

--- src/assignment3_june_13/null.kt
+++ src/assignment3_june_13/null.kt
@@ -1,38 +1,33 @@
+package assignment3_june_13
+
 fun main()
 {
     // 1.
     var username: String? = "JohnDoe"
-    println("1. Length of username: ${username?.length}")
-
+    println("Length of username: ${username?.length}")
     // 2.
     fun getLength(input: String?): Int {
         return input?.length ?: 0
     }
-    println("2. Length from getLength(): ${getLength("Kotlin")}")
-    println("2. Null input to getLength(): ${getLength(null)}")
-
+    println(" Length from getLength(): ${getLength("Kotlin")}")
+    println(" Null input to getLength(): ${getLength(null)}")
     // 3.
     val name: String? = "Nitish"
     name?.let {
-        println("3. Name in uppercase: ${it.uppercase()}")
+        println("Name in uppercase: ${it.uppercase()}")
     }
-
     // 4.
     val email: String? = null
-    println("4. Email: ${email ?: "No email provided"}")
-
+    println("Email: ${email ?: "No email provided"}")
     // 5.
     val data: Any = 25
     val age: Int? = data as? Int
-    println("5. Safely casted age: $age")
-
+    println("Safely casted age: $age")
     // 6 & 7
     var address: String? = null
     val len = address?.length ?: 0
-    println("6. Length of address: $len")
-
+    println("Length of address: $len")
     // 8.
-
     // 9.
     val token: String? = "ABC123"
     token?.run {
